3fc1888484531a1a23b880f9875614e56f846564
Max <maaax.fischer@gmail.com>
2022-11-30 00:24:01 +0100

    Add link to question

--- python-solutions/binary/add_binary.py
+++ python-solutions/binary/add_binary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# My Solution to https://leetcode.com/problems/add-binary/
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a_dec = int("0b" + a, 2)
